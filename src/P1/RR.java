b43c5b7f5f1679610dd6de481c468e514d4ad6fd
Zachary Milrod <zmilrod@gmail.com>
2016-02-29 16:37:27 -0500

    started RR

---
+++ src/P1/RR.java
@@ -0,0 +1,24 @@
+package P1;
+
+import java.util.*;
+
+
+public class RR {
+	Queue<Job> readyQueue;
+
+	public RR(){
+		readyQueue = = new LinkedList<Job>();
+		
+	}
+
+	public void run(){
+		while(!q.isEmpty()){
+            Job current = q.remove();
+            
+        }
+	}
+
+
+
+
+}
