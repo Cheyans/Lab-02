7c388031700d4240ac531c68dae5ccf24136ed22
Benjamin <e>
2016-02-29 16:39:50 -0500

    Finished FCFS, need debugging/testing

--- src/P1/FCFS.java
+++ src/P1/FCFS.java
@@ -2,12 +2,31 @@ import java.util.*;
 
 
 public class FCFS {
-	Queue<Job> q = new LinkedList<Job>();
-	public void run(){
+	// Queue<Job> q = new LinkedList<Job>();
+ //    Final String fileName; 
+ //    List<Job> list;
+    // public FCFS (String fileName, List<Job> list){
+    //     this.fileName = finalName;
+    //     this.list = list;
+    // }
+
+	public static int run(List<Job> list){
+        Queue<Job> q = new LinkedList<Job>();
+        for(Job jobs : list)
+            q.add(jobs);
+        //all jobs added to q in order
 		while(!q.isEmpty()){
             Job current = q.remove();
-            
+            for(Job job : q){
+                job.waitTime += current.length;
+            }
         }
+        int total = 0;
+        for(Job job : list)
+            total += job.waitTime;
+        double size = (double) list.size();
+        double average = total/size;
+        return average;
 	}
 
 
