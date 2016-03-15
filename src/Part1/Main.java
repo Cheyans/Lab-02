197795e783feea73cdea8fe0ce652532cae0d527
cheyans <cheyan.set@gmail.com>
2016-03-15 12:47:17 -0400

    comments for p1

--- src/Part1/Main.java
+++ src/Part1/Main.java
@@ -6,21 +6,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-		//Get all files in the traces folder and add them to the path list
+        //Get all files in the traces folder and add them to the path list
         List<Path> paths = new ArrayList<>();
         Files.walk(Paths.get(Main.class.getResource("Traces").toURI())).forEach(filePath -> {
             if (Files.isRegularFile(filePath)) {
                 paths.add(filePath);
             }
         });
-		
-		//Sort paths based on their numeric file names by comparing the 3 comparable integers in the filenames
+
+        //Sort paths based on their numeric file names by comparing the 3 comparable integers in the filenames
         Collections.sort(paths, (path1, path2) -> {
-			//Extract integers from path name
+            //Extract integers from path name
             String[] path1Names = path1.getFileName().toString().split("-|\\.");
             String[] path2Names = path2.getFileName().toString().split("-|\\.");
             int comparison = 0;
-			//Compring the 3 integers
+            //Compring the 3 integers
             for (int i = 0; i < path1Names.length; i++) {
                 comparison = Integer.compare(Integer.parseInt(path1Names[i]), Integer.parseInt(path2Names[i]));
                 if (comparison != 0) {
@@ -30,13 +30,13 @@ public class Main {
             //Should never happen, means the file has the same name or an improper naming scheme
             return comparison;
         });
-		//Run the appropriate schedulers
+        //Run the appropriate schedulers
         runScheduler(paths, new FCFS());
         runScheduler(paths, new RR());
     }
 
     private static void runScheduler(List<Path> paths, Scheduler scheduler) throws IOException {
-		//Runs all jobs in all paths for provided scheduler
+        //Runs all jobs in all paths for provided scheduler
         for (Path path : paths) {
             List<Job> jobs = new LinkedList<>();
             Files.readAllLines(path).forEach(line -> {
@@ -45,6 +45,8 @@ public class Main {
                     jobs.add(new Job(Integer.parseInt(values[0]), Integer.parseInt(values[1])));
                 }
             });
+            //order by arrival time to make FCFS simpler
+            Collections.sort(jobs, (o1, o2) -> Integer.compare(o1.arrivalTime, o2.arrivalTime));
             System.out.println(String.format("%s %s: %.0f", scheduler.getName(), path.getFileName().toString().split("\\.")[0], scheduler.run(jobs)));
         }
     }
@@ -74,14 +76,15 @@ class FCFS implements Scheduler{
     @Override
     public double run(List<Job> jobs){
         int jobCount = jobs.size();
-		//takes the list of jobs as a queue
+        //takes the list of jobs as a queue
         Queue<Job> q = (Queue<Job>) jobs;
         Job prev = null;
         double total = 0;
-		
-		//iterates of queue until everything is empty
+
+        //iterates of queue until everything is empty
         while(!q.isEmpty()){
             Job completed = q.remove();
+            //run job and increase its wait time and total current time
             if(prev != null) {
                 completed.waitTime = prev.waitTime + prev.length;
                 total += completed.waitTime;
@@ -105,10 +108,11 @@ class RR implements Scheduler{
         int jobCount = jobs.size();
         Queue<Job> readyQueue = new LinkedList<>();
         int time = 0;
-        double avgWaitTime = 0;
+        double totalWaitTime = 0;
 
         while (!jobs.isEmpty()) {
 
+            //iterate over all jobs and check if they are still in queue
             for(Job job: jobs) {
                 if(job.arrivalTime == time) {
                     readyQueue.add(job);
@@ -116,6 +120,7 @@ class RR implements Scheduler{
                 }
             }
 
+            //run all currently arrived jobs
             if (!readyQueue.isEmpty()) {
                 Job currJob = readyQueue.remove();
                 currJob.inQueue = false;
@@ -124,17 +129,19 @@ class RR implements Scheduler{
                 jobs.stream().filter(job -> job.inQueue).forEach(job -> job.waitTime++);
                 currJob.progress++;
 
+                //if job has finished increase total wait time and pop the job off the list
                 if (currJob.length == currJob.progress) {
-                    avgWaitTime += currJob.waitTime;
+                    totalWaitTime += currJob.waitTime;
                     jobs.remove(currJob);
                 } else {
+                    //else keep job in ready queue
                     readyQueue.add(currJob);
                     currJob.inQueue = true;
                 }
             }
             time++;
         }
-        return Math.ceil(avgWaitTime / jobCount);
+        return Math.ceil(totalWaitTime / jobCount);
     }
 
     @Override
